da4e2604f917eafb235b5fe87a81fb1a8b9a111e
naheyansheikh <naheyan@students.cs.ubc.ca>
2024-05-12 20:33:10 -0700

    initialcommit

---
+++ src/components/features/about/about.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Headings } from '../../core/headings/headings'
+import './style.css'
+
+const About = React.forwardRef<HTMLDivElement>((props, ref) => {
+  return (
+    <div ref={ref} className="about-section">
+      {/* 🎯 Display the section title and subtitle */}
+      <Headings title="About" subtitle="Hi!👋 I'm Naheyan." />{' '}
+      {/* 📝 Edit the biography below to reflect your own journey and interests */}
+      <p className="about-text">
+        I&apos;m currently in my third year at the University of British Columbia, 
+        pursuing a degree in Computer Science. My journey into the world of programming began with coursework, 
+        where I delved into languages like Java, JavaScript, C, C++, and Python. However, my thirst for knowledge led me 
+        to explore beyond the classroom, diving into self-learning to deepen my understanding and mastery of these languages.<br />
+
+        What truly sets my heart racing is AI Development. The potential of artificial intelligence to revolutionize industries 
+        and improve lives is what drives my passion. I&apos;m dedicated to shaping my academic path and future career towards this fascinating field.<br />
+
+        In addition to my academic pursuits, I&apos;m an avid creator. I&apos;ve embarked on personal projects that tackle my everyday challenges, honing my skills 
+        and creativity. These ventures have allowed me to apply my knowledge in practical scenarios and explore the endless possibilities technology offers.
+      </p>
+    </div>
+  )
+})
+
+About.displayName = 'About'
+
+export { About }
---
+++ src/library/data.ts
@@ -0,0 +1,63 @@
+// 📦 Projects list: Customize with your own project details.
+export const projects = [
+  {
+    title: 'Sport Scheduler', // Enter project title.
+    githubUrl: 'https://github.com/naheyansheikh/cpsc210-project', // Enter GitHub URL.
+    externalUrl: '', // Enter live site URL.
+    languages: ['Java', 'GUI'], // Specify technologies used.
+  },
+  {
+    title: 'Fitted Closet',
+    githubUrl: 'https://github.com/naheyansheikh/fitted-closet',
+    externalUrl: '',
+    languages: ['JavaScript', 'CSS'],
+  },
+  {
+    title: 'Portfolio',
+    githubUrl: '',
+    externalUrl: '',
+    languages: ['Node.js', 'TypeScript', 'CSS'],
+  },
+  {
+    title: 'Chat App',
+    githubUrl: '',
+    externalUrl: '',
+    languages: ['React', 'TypeScript', 'Firebase'],
+  },
+  {
+    title: 'Netflix Clone',
+    githubUrl: '',
+    externalUrl: '',
+    languages: ['React', 'TypeScript', 'Firebase'],
+  },
+  {
+    title: 'Instagram Clone',
+    githubUrl: '',
+    externalUrl: '',
+    languages: ['React', 'TypeScript', 'CSS', 'Firebase'],
+  },
+]
+
+// 📱 Social media links: Update with your URLs and choose appropriate icons.
+export const socialLinks = [
+  {
+    name: 'GitHub',
+    url: 'https://github.com/naheyansheikh', // Enter GitHub link.
+    icon: 'Github',
+  },
+  {
+    name: 'LinkedIn',
+    url: 'https://www.linkedin.com/in/naheyan-sheikh-6719b6243/', // Enter GitHub link.
+    icon: 'LinkedIn',
+  },
+]
+
+// 🚀 Navigation data: Customize navigation labels and links as needed.
+export const navbarData = {
+  navigator: [
+    { name: 'Home' },
+    { name: 'About' },
+    { name: 'Projects' },
+    { name: 'Contact' },
+  ],
+}
